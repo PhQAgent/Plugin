cf57c87f4bc706b7969bf14de17c40294f004bf7
Aodzip <aodzip@gmail.com>
2016-08-12 08:08:01 +0800

    加了几个功能

--- Stats.php
+++ Stats.php
@@ -13,11 +13,14 @@ class Stats extends PluginBase{
 
     private function getStats($m, $player){
         $curl = new Curl();
-        $html = $curl->setUrl("https://osu.ppy.sh/u/$player")->exec();
-        preg_match('/var userId = (.*);/iU', $html, $userid);
+        $html_o = $curl->setUrl("https://osu.ppy.sh/u/$player")->exec();
+        preg_match('/var userId = (.*);/iU', $html_o, $userid);
         if(!isset($userid[1]) or (trim($userid[1]) == null)){
             return false;
         }
+        $ua = explode('<div class="profile-username">', $html_o);
+        $ub = explode('</div>', $ua[1]);
+        $username = $ub[0];
         $userid = $userid[1];
         $html = $curl->setUrl("https://osu.ppy.sh/pages/include/profile-general.php")->
         setGet([
@@ -37,13 +40,70 @@ class Stats extends PluginBase{
             preg_match('/pp \(#(.*)\)/iU', $html, $rank);
             $rank = $rank[1];
         }
+        preg_match('/<div title=\'Arrived\'>.*datetime.*>(.*)</iU', $html_o, $at);
+        $at = $this->timediff(strtotime($at[1]), time());
+        preg_match('/<div title=\'Last Active\'>.*datetime.*>(.*)</iU', $html_o, $la);
+        $la = $this->timediff(strtotime($la[1]), time());
         preg_match('/Hit Accuracy<\/b>: (.*)%/iU', $html, $acc);
         $acc = $acc[1];
-        preg_match('/Total Hits<\/b>: (.*)<\/div>/iU', $html, $tth);
-        $tth = $tth[1];
+        preg_match('/Play Count<\/b>: (.*)<\/div>/iU', $html, $pc);
+        $pc = $pc[1];
         preg_match('/Play Time<\/b>: (.*)<\/div>/iU', $html, $pt);
         $pt = $pt[1];
-        return "Performance: {$pp}pp\nRank: #{$rank}\nHit Accuracy: {$acc}%\nPlay Time: {$pt}\nTotal Hits: {$tth}";
+        preg_match('/Current Level<\/b>: (.*)<\/div>/iU', $html, $cv);
+        $cv = $cv[1];
+        preg_match('/Total Hits<\/b>: (.*)<\/div>/iU', $html, $tth);
+        $tth = $tth[1];
+        preg_match('/Maximum Combo<\/b>: (.*)<\/div>/iU', $html, $mc);
+        $mc = $mc[1];
+        preg_match('/X.png.*width.*>(.*)</iU', $html, $ss);
+        $ss = $ss[1];
+        preg_match('/S.png.*width.*>(.*)</iU', $html, $s);
+        $s = $s[1];
+        preg_match('/A.png.*width.*>(.*)</iU', $html, $a);
+        $a = $a[1];
+        $strat = '';
+        foreach($at as $key => $value){
+            if($value > 0){
+                if($value > 1) $key_r = $key . 's';
+                $strat .= "$value $key_r ";
+                if($key == 'year' and $value >= 2) break;
+                if($key == 'day' and $value >= 15) break;
+                if($key == 'min' and $value >= 10) break;
+            }
+        }
+        $strla = '';
+        foreach($la as $key => $value){
+            if($value > 0){
+                if($value > 1) $key_r = $key .'s';
+                $strla .= "$value $key_r ";
+                if($key == 'year' and $value >= 2) break;
+                if($key == 'day' and $value >= 15) break;
+                if($key == 'min' and $value >= 10) break;
+            }
+        }
+        $modemap = ['Std', 'Taiko', 'Ctb', 'Mania'];
+        $mode = $modemap[$m];
+        return "Account: $username\nMode: $mode\nPerformance: {$pp}pp\nRank: #{$rank}\nHit Accuracy: {$acc}%\nPlay Count: {$pc}\nPlay Time: {$pt}\nCurrent Level: {$cv}\nTotal Hits: {$tth}\nMaximum Combo: {$mc}\nSS: {$ss} S: {$s} A: {$a}";
+    }
+
+    public function timediff($a, $b){  
+        $diff = max([$a, $b]) - min([$a, $b]);
+        $days = (int)($diff / 86400);
+        $years = (int)($days / 365);
+        $days = $years % 365;
+        $remain = $diff % 86400;
+        $hours = (int)($remain / 3600);
+        $remain = $remain % 3600;
+        $mins = (int)($remain / 60);
+        $secs = $remain % 60;
+        return [
+            'year' => $years,
+            'day' => $days,
+            'hour' => $hours,
+            'min' => $mins,
+            'sec' => $secs
+        ];
     }
 
     public function onReceive(Message $message){
@@ -52,7 +112,7 @@ class Stats extends PluginBase{
             $arg = isset($arg[1]) ? $arg[1] : '';
             $rs = $this->getStats(0, $arg);
             if($rs){
-                $msg = "Account: $arg\nMode: Std\n$rs";
+                $msg = $rs;
             }else{
                 $msg = "账户 $arg 不存在";
             }
@@ -65,7 +125,7 @@ class Stats extends PluginBase{
             $arg = isset($arg[1]) ? $arg[1] : '';
             $rs = $this->getStats(1, $arg);
             if($rs){
-                $msg = "Account: $arg\nMode: Taiko\n$rs";
+                $msg = $rs;
             }else{
                 $msg = "账户 $arg 不存在";
             }
@@ -78,7 +138,7 @@ class Stats extends PluginBase{
             $arg = isset($arg[1]) ? $arg[1] : '';
             $rs = $this->getStats(2, $arg);
             if($rs){
-                $msg = "Account: $arg\nMode: Ctb\n$rs";
+                $msg = $rs;
             }else{
                 $msg = "账户 $arg 不存在";
             }
@@ -89,9 +149,9 @@ class Stats extends PluginBase{
         if(strstr($message->getContent(), '!mania ')){
             $arg = explode('!mania ', $message->getContent());
             $arg = isset($arg[1]) ? $arg[1] : '';
-            $rs = $this->getStats(1, $arg);
+            $rs = $this->getStats(3, $arg);
             if($rs){
-                $msg = "Account: $arg\nMode: Mania\n$rs";
+                $msg = $rs;
             }else{
                 $msg = "账户 $arg 不存在";
             }
